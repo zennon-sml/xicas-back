04b3349611df46b8a9f24cfa7d18a47ef0b56065
X1K00 <fcju14@gmail.com>
2025-02-24 20:09:30 -0300

    creating the route to search by barcode

--- controllers/product.js
+++ controllers/product.js
@@ -33,6 +33,20 @@ export const getProductById = async (req, res) => {
   }
 }
 
+export const getProductByBarcode = async (req, res) => {
+  const { barcode } = req.params;
+  try {
+    const product = await Product.findOne({ where: { barcode } });
+    if (product) {
+      res.status(200).json(product);
+    } else {
+      res.status(404).json({ error: "Produto não encontrado" });
+    }
+  } catch (error) {
+    res.status(400).json({ error: error.message || "Erro ao listar produto único" });
+  }
+}
+
 export const updateProductById = async (req, res) => {
   const { id } = req.params;
   const { name, barcode, description, price, cost, image } = req.body;
